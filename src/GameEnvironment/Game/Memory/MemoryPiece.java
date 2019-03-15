c150e1ebd7f48c0d483e643f72adc57d2f8147e1
Tam <aliencheez12@gmail.com>
2019-03-14 17:06:56 -0700

    Added image assets

--- src/GameEnvironment/Game/Memory/MemoryPiece.java
+++ src/GameEnvironment/Game/Memory/MemoryPiece.java
@@ -7,7 +7,14 @@ import GameEnvironment.Piece;
 
 class MemoryPiece extends Piece {
 	private static final String gameFolder = System.getProperty("user.dir") + "\\GameEnvironment\\Game\\TicTacToe\\";
-	private final String[] pieceIcon = {gameFolder + "X.png", gameFolder + "O.png"};
+	private final String[] pieceIcon = {gameFolder + "1.png", gameFolder + "2.png", gameFolder + "3.png", gameFolder + "4.png",
+										gameFolder + "5.png", gameFolder + "6.png", gameFolder + "A.png", gameFolder + "B.png",
+										gameFolder + "C.png", gameFolder + "D.png", gameFolder + "E.png", gameFolder + "F.png",
+										gameFolder + "G.png", gameFolder + "H.png", gameFolder + "I.png", gameFolder + "J.png",
+										gameFolder + "K.png", gameFolder + "L.png", gameFolder + "M.png", gameFolder + "N.png",
+										gameFolder + "O.png", gameFolder + "P.png", gameFolder + "Q.png", gameFolder + "R.png",
+										gameFolder + "S.png", gameFolder + "T.png", gameFolder + "U.png", gameFolder + "V.png",
+										gameFolder + "W.png", gameFolder + "X.png", gameFolder + "Y.png", gameFolder + "Z.png"};
 	
 	protected MemoryPiece(String pieceName, int player) {
 		super(pieceName, player);
